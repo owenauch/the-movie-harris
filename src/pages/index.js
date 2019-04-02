2ddf3ea815bc4951eb26eca4124f849138267689
Owen Auch <owenauch@wustl.edu>
2019-04-02 14:03:06 -0500

    review template done, starting on main list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,50 @@
 import React from "react"
 
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" keywords={[`movies`, `Harrison Steedman`, `film`, `reviews`]} />
+    <SEO
+      title="Home"
+      keywords={[`movies`, `Harrison Steedman`, `film`, `reviews`]}
+    />
     <h1>Reviews</h1>
-    <p>Blog posts would go under here.</p>
+    {console.log(data)}
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <div key={node.id}>
+        <Link to={node.fields.slug}>
+          <h3>
+            {node.frontmatter.title} <span>— {node.frontmatter.date}</span>
+          </h3>
+          <p>{node.excerpt}</p>
+        </Link>
+      </div>
+    ))}
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+            rating
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
---
+++ src/templates/review.js
@@ -0,0 +1,62 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+class ReviewTemplate extends React.Component {
+  render() {
+    const review = this.props.data.markdownRemark
+    console.log(review)
+    const siteTitle = this.props.data.site.siteMetadata.title
+    const { previous, next } = this.props.pageContext
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO
+          title={review.frontmatter.title}
+          description={review.frontmatter.description || review.excerpt}
+        />
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "flex-start"}}>
+          <h1>{review.frontmatter.title}</h1>
+          <p style={{ marginLeft: ".8rem", marginRight: ".8rem"}}>|</p>
+          {/* make red if below 5, green if above 5 */}
+          {review.frontmatter.rating < 5 ?
+            <h2>
+              <span style={{ color: "#e51937"}}>{review.frontmatter.rating}</span>
+              /10
+            </h2> : 
+            <h2>
+              <span style={{ color: "#2CB714"}}>{review.frontmatter.rating}</span>
+              /10
+            </h2>
+          }
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: review.html }} />
+      </Layout>
+    )
+  }
+}
+
+export default ReviewTemplate
+
+export const pageQuery = graphql`
+  query ReviewBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      excerpt(pruneLength: 160)
+      html
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+        rating
+      }
+    }
+  }
+`
