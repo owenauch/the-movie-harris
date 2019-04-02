8632806e6bc6f415bd51c8ef0675f1c47a2d8011
bamoran <49212732+bamoran@users.noreply.github.com>
2019-04-02 17:38:57 -0400

    Update review.js

--- src/templates/review.js
+++ src/templates/review.js
@@ -31,6 +31,11 @@ class ReviewTemplate extends React.Component {
           }
         </div>
         <div dangerouslySetInnerHTML={{ __html: review.html }} />
+        <a class="twitter-share-button"
+          href="https://twitter.com/intent/tweet?text=Hello%20world"
+          data-size="large">
+          Tweet
+        </a>
       </Layout>
     )
   }
